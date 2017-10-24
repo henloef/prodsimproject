9abc8bc1dde3dc881c926e396658a6533c7fe872
henloef <henloef@gmail.com>
2017-10-24 13:52:59 +0200

    add list of x and y coordinates

--- circular_arch.m
+++ circular_arch.m
@@ -4,10 +4,24 @@ H = 400; %mm height
 h = 10; %mm thickness
 n_elements = 20; %number of beam elements
 L = 1600; %mm distance between hinges
-theta_L = 0; %rad theta when (x,y) = (L,0)
-theta_0 = 0; %rad theta when (x,y) = (0,0)
+E = 2.1e5; %N/mm^2 elastic modulus
+nu = 0; % poisson's ratio
 
-theta_L = asin((R-H)/R);
-theta_0 = pi - theta_L;
-delta_theta = (theta_0 - theta_L)/n_elements;
+theta_L = asin((R-H)/R); %rad theta when (x,y) = (L,0)
+theta_0 = pi - theta_L; %rad theta when (x,y) = (0,0)
 
+delta_theta = (theta_0 - theta_L)/n_elements; %difference in arc positions of each beam element
+
+theta_list = zeros(21,1);
+
+for i = 1:21
+    theta_list(i) = theta_0 - delta_theta*(i-1); %list containing angles representing points along the arch
+end
+
+beam_coords = zeros(length(theta_list), 2); %list containing all (x,y) coordinates of the beams
+
+for j = 1:length(theta_list)
+    beam_coords(j,1) = R*cos(theta_list(j)) + 0.5*L;  %calculates x coordinate based on angle 
+    beam_coords(j,2) = R*sin(theta_list(j)) - (R-H);
+end
+    
