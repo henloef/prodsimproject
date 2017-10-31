1dcd4b042027557f5fea90109c14c5f422d4e393
Martin Loeland <martinloland@gmail.com>
2017-10-31 21:42:45 +0100

    made global stiffness function more robust

--- global_stiffness.m
+++ global_stiffness.m
@@ -9,13 +9,11 @@ function [ K ] = global_stiffness( Edof, Coord, ep )
 %         ep = [E A I] modulus of elasticity, area, moment of inertia
 % OUTPUT: K : global stiffness matrix
 %--------------------------------------------------------------------
-
-total_dof = 21*3;
-
+total_dof = size(Coord,1)*3;
+n_elements = size(Edof,1);
 K = zeros(total_dof);
 
-
-for i=1:20
+for i=1:n_elements
     % Elements stiffness matrix
     ex = transpose(Coord(i:i+1,1));
     ey = transpose(Coord(i:i+1,2));
@@ -23,4 +21,3 @@ for i=1:20
     % Add to global stiffness matrix
     K = assem(Edof(i,:), K, Ke);
 end
-
