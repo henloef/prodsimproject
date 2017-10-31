6b73076d15e861d7015dd4cd11528e16aa8c1416
Martin Loeland <martinloland@gmail.com>
2017-10-31 11:42:21 +0100

    changed to rectanuglar properties

--- area_properties.m
+++ area_properties.m
@@ -1,16 +1,17 @@
-function [ A, I ] = area_properties( d )
+function [ A, I ] = area_properties( t, w )
 % [A, I]=area_properties(d)
 %--------------------------------------------------------------------
 % PURPOSE
 %  Returns the area and second moment of inertia of a circular section
 %
-% INPUT:  d = diameter mm
+% INPUT:  t = thickness
+%         w = width
 % OUTPUT: A : area mm^2
 %         I : moment of intertia mm^4
 %--------------------------------------------------------------------
-r = d/2;
-A = pi*r^2;
-I = (pi/4)*r^4;
+
+A = t*w;
+I = (1/12)*w*t^3;
 
 end
 
