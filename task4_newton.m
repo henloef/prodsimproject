40a819b35786861aaabf591395863b9e45692425
Martin Loeland <martinloland@gmail.com>
2017-11-14 13:55:59 +0100

    task 4 is now working

--- task4_newton.m
+++ task4_newton.m
@@ -5,29 +5,30 @@ E = 2.1e5; %% N/mm^2
 thickness = 10; % mm
 width = 100; % mm
 H = 400; % height mm
-max_load = -2.9e4; % N
+[A, I] = area_properties(thickness, width);
+ep = [E A I];
 
-increments = 20;
-max_iterations = 30;
-max_residual = 0.1;
-iteration = 1;
+%% Increments and iterations
+increments = 50;
+max_iterations = 20;
+max_residual = 0.01;
 
-f_magnitude = [0:max_load/increments: max_load];
-residual_plot = zeros(increments,max_iterations);
+%% Load
+max_load = -26.7e3; % N
+f_magnitude = 0 : max_load/(increments-1) : max_load;
+
+%% Placeholders for plots
+r_plot = zeros(increments,max_iterations);
+u_plot = zeros(increments,1);
 
 %% Create geometry
 [Edof, Coord_0,  Dof] = circular_arch(20);
 total_dof = size(Coord_0,1)*3;
-
-%% Section and material properties
-[A, I] = area_properties(thickness, width);
-ep = [E A I];
-
-%% Boundary conditions
 bc = [1 0; 2 0; 61 0; 62 0];
 
 %% Solve
 a = zeros(total_dof,1);
+iteration = 1;
 for i=1:increments
     f = load_vector(Edof, f_magnitude(i));
     while iteration <= max_iterations
@@ -39,14 +40,18 @@ for i=1:increments
         
         % Check threshold for residual
         r_sum = sqrt(r'*r); %Root sum squared
-        residual_plot(i, iteration) = r_sum;
+        r_plot(i, iteration) = r_sum;
         if r_sum < max_residual
             break
         end
         iteration = iteration + 1;
     end
+    u_plot(i) = max(abs(a));
     iteration = 1;
 end
+
+%% Plots
+title_prefix = 'Newton iteration,';
 
 %% Plot deformed
 Coord = new_coord(Coord_0,a);
@@ -55,17 +60,23 @@ plotpar = [2 2 1];
 sfac = 1;
 Ed = extract(Edof, a);
 eldisp2(Ex, Ey, Ed, plotpar, sfac);
+title(strcat(title_prefix, ' geometry last increment'))
+saveas(gcf,'../fig/task4_geometry.png')
 
+%% Plot residual
 figure
-plot([1:1:max_iterations],residual_plot(3,:))
-% axis([0 max_iterations 0 0.1])
+plot([1:1:max_iterations],r_plot(3,:))
+title(strcat(title_prefix, ' residual last increment'))
+xlabel('iteration')
+ylabel('residual [N]')
+grid on
+saveas(gcf,'../fig/task4_residual.png')
 
-% figure
-% plot(displacement, abs(load))
-% xlabel('displacement [mm]')
-% ylabel('load [N]')
-% 
-% figure
-% plot(displacement, residual)
-% xlabel('displacement [mm]')
-% ylabel('internal force [N]')
+%% Plot load displacement
+figure
+plot(u_plot,abs(f_magnitude))
+title(strcat(title_prefix, ' load/displacement'))
+xlabel('displacement [mm]')
+ylabel('load [N]')
+grid on
+saveas(gcf,'../fig/task4_load_disp.png')
