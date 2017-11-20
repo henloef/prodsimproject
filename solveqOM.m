13eb0c2ddf28c28e11a59e8c92818b03826a70ae
olemfyen <ole.m.fyen@gmail.com>
2017-11-20 10:28:09 +0100

    add solveqOM

---
+++ solveqOM.m
@@ -0,0 +1,21 @@
+function [d] = solveqOM(K,q,bc)
+
+[nd,nd]=size(K);
+     fdof=[1:nd]';
+%
+     d=zeros(size(fdof));
+   
+%
+     pdof=bc(:,1);
+     dp=bc(:,2);
+     fdof(pdof)=[];
+%
+     s=K(fdof,fdof)\(q(fdof)-K(fdof,pdof)*dp);
+%
+     d(pdof)=dp;
+     d(fdof)=s;
+
+end
+
+
+
